fa1f65112705ba2e5d73eacafcd5f1a3991569ee
Bolsyn <Serikagis@yandex.ru>
2021-03-03 20:45:41 +0600

    Add Auth and SaveToProfile

--- ConnectedLabLesson/Service/AuthService.cs
+++ ConnectedLabLesson/Service/AuthService.cs
@@ -1,12 +1,17 @@
-﻿using ConnectedLabLesson.Model;
+﻿using ConnectedLabLesson.Data;
+using ConnectedLabLesson.Model;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace ConnectedLabLesson.Service
 {
     public class AuthService
     {
+        ushort secretKey = 0x0088;
+
         public Account ChooseMenu()
         {
             Console.WriteLine("Регистрация? Нажмите 1");
@@ -16,29 +21,86 @@ namespace ConnectedLabLesson.Service
 
             if (signUp == 1)
             {
+                var accountDataAccess = new AccountDataAccess();
+                var accounts = accountDataAccess.Select();
+
+                bool IsUnUsed = true;
+                
                 var account = new Account();
                 Console.Write("Напишите Логин: ");
                 account.Login = Console.ReadLine();
+                foreach (var acc in accounts)
+                {
+                    if (acc.Login == account.Login)
+                    {
+                        IsUnUsed = false;
+                    }
+                }
+                if (!IsUnUsed)
+                {
+                    Console.WriteLine( "Такой Логин существует!");
+                    return null;
+                }
                 Console.Write("Напишите Пароль: ");
-                account.Password = Console.ReadLine();
+                string clearPass = Console.ReadLine();
+                account.Password = EncodeDecrypt(clearPass, secretKey);
+
+                accountDataAccess.Insert(account);
             }
             else if (signUp == 2)
             {
+                var accountDataAccess = new AccountDataAccess();
+                var accounts = accountDataAccess.Select();
+                
                 Console.Write("Логин: ");
                 var loginOfEnter = Console.ReadLine();
                 Console.Write("Пароль: ");
                 var passwordOfEnter = Console.ReadLine();
+                foreach (var acc in accounts)
+                {
+                    if (acc.Login == loginOfEnter)
+                    {
+                        if (EncodeDecrypt(acc.Password, secretKey) == passwordOfEnter)
+                        {
+                            return acc;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Неправльиный пароль");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неправльиный логин");
+                    }
+                }
             }
             else if (signUp == 0)
             {
-                Console.WriteLine("Пока");
+                Console.WriteLine("До свидания!");
                 return null;
             }
             else 
-            { 
+            {
+                Console.WriteLine("Нет такого пункта меню");
                 return null; 
             }
             return null;
+        }
+
+        public static string EncodeDecrypt(string str, ushort secretKey)
+        {
+            var ch = str.ToArray(); 
+            string newStr = "";      
+            foreach (var c in ch)  
+                newStr += TopSecret(c, secretKey);  
+            return newStr;
+        }
+
+        public static char TopSecret(char character, ushort secretKey)
+        {
+            character = (char)(character ^ secretKey); 
+            return character;
         }
     }
 }
---
+++ ConnectedLabLesson/Service/SaveProfileService.cs
@@ -0,0 +1,30 @@
+﻿using ConnectedLabLesson.Data;
+using ConnectedLabLesson.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConnectedLabLesson.Service
+{
+    public class SaveProfileService
+    {
+        public Profile SaveToProfile(Account account)
+        {
+            var profileDataAccess = new ProfileDataAccess();
+
+            var profile = new Profile();
+            profile.Login = account.Login;
+            Console.Write("Напишите Имя: ");
+            profile.FirstName = Console.ReadLine();
+            Console.Write("Напишите Фамилию: ");
+            profile.LastName = Console.ReadLine();
+            Console.Write("Напишите Email: ");
+            profile.Email = Console.ReadLine();
+            Console.Write("Напишите путь к картинке: ");
+            profile.PathToImage = Console.ReadLine();
+
+            profileDataAccess.Insert(profile);
+            return profile;
+        }
+    }
+}
