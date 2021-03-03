822d43dc5bfc548d8f4ad866ec4088a6dbc9969a
Anuar <beisekeyev.a.1992.ast@gmail.com>
2021-03-03 21:31:44 +0600

    Added IComparable interface

--- ConnectedLabLesson/Model/Account.cs
+++ ConnectedLabLesson/Model/Account.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace ConnectedLabLesson.Model
 {
-    public class Account
+    public class Account : IComparable<Account>
     {
         public string Login { get; set; }
         public string Password { get; set; }
+
+        public int CompareTo( Account other)
+        {
+            return this.Login.CompareTo(other.Login);
+        }
     }
 }
--- ConnectedLabLesson/Model/Profile.cs
+++ ConnectedLabLesson/Model/Profile.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace ConnectedLabLesson.Model
 {
-    public class Profile
+    public class Profile : IComparable<Profile>
     {
         public string Login { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string PathToImage { get; set; }
+
+        public int CompareTo(Profile other)
+        {
+            return this.Login.CompareTo(other.Login);
+        }
     }
 }
