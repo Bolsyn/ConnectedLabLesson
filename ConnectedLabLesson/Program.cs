c60abe242b8cb45d8d349cc6b6cfe276fb732f46
Bolsyn <Serikagis@yandex.ru>
2021-03-03 19:16:25 +0600

    Add project files.

---
+++ ConnectedLabLesson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConnectedLabLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
