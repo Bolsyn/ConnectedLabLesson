2cf18b94fd1fd6bc51dd3a12a5d9a97ebe434113
Anuar <beisekeyev.a.1992.ast@gmail.com>
2021-03-03 19:40:10 +0600

    Added DbDataAccess class

---
+++ ConnectedLabLesson/Data/DbDataAccess.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Common;
+using ConnectedLabLesson.Service;
+
+namespace ConnectedLabLesson.Data
+{
+    public abstract class DbDataAccess<T> : IDisposable
+    {
+        protected readonly DbProviderFactory factory;
+        protected readonly DbConnection connection;
+        public DbDataAccess()
+        {
+            factory = DbProviderFactories.GetFactory("ConnectedProvider");
+            connection = factory.CreateConnection();
+            connection.ConnectionString = ConfigurationService.Configuration["DataAccessConnectionString"];
+            connection.Open();
+        }
+        public abstract void Insert(T entity);
+        public abstract void Update(T entity);
+        public abstract void Delete(T entity);
+        public abstract ICollection<T> Select();
+        public abstract ICollection<T> Select(string value);
+        public void Dispose()
+        {
+            connection.Close();
+        }
+    }
+}
---
+++ ConnectedLabLesson/Service/ConfigurationService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ConnectedLabLesson.Service
+{
+    public static class ConfigurationService
+    {
+        public static IConfiguration Configuration { get; private set; }
+        public static void Init()
+        {
+            if (DbProviderFactories.GetFactory("ConnectedProvider") == null)
+            {
+                DbProviderFactories.RegisterFactory("ConnectedProvider", SqlClientFactory.Instance);
+            }
+            if (Configuration == null)
+            {
+                var configuratonBuilder = new ConfigurationBuilder();
+                var configuration = configuratonBuilder.AddJsonFile("appSetting.json").Build();
+            }
+        }
+    }
+}
